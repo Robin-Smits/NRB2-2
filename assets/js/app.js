2718e223dd5969fd421cb9cf91b481423d32b4d2
Robin-Smits <smit0141@hz.nl>
2022-10-31 16:31:42 +0100

    file

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,7 @@ const addComment = document.getElementById('comments')
 const commentArray = [];
 const firstName = 'robin';
 const lastName = 'smits';
+const userComment = 'ik vind dit goed'
 
 riversButton.addEventListener('click', changeMapToRivers);
 damsButton.addEventListener('click', changeMapTodams);
@@ -14,14 +15,17 @@ areaButton.addEventListener('click', changeMapToarea);
 function changeMapToRivers (){console.log('rivers works')};
 function changeMapTodams (){console.log('dams works')};
 function changeMapToarea (){console.log('area works')};
-function addComments (){
+function addComments (firstName, lastName, userComment){
+    commentDiv = document.createElement('div');
     src = document.getElementById('displayUserInput');
-    
-    div = document.createElement('div');
-    src.appendchild(div)
-
-    p = document.createElement('p');
-    p.innerHTML = `${firstName} ${lastName}`
-    div.appendchild(p);
+    src.appendChild(commentDiv);
+    //p
+    commentUserName = document.createElement('p');
+    commentUserName.innerHTML = `${firstName} ${lastName}`
+    commentDiv.appendChild(commentUserName);
+    //p
+    displayUserComment = document.createElement('p');
+    displayUserComment = userComment;
+    commentDiv.appendChild(displayUserComment);
 }
 addComments ();
