c75232bdaeb3b37f9435dd2e7032832ac0c819a5
Robin-Smits <smit0141@hz.nl>
2022-11-01 09:57:03 +0100

    addComment

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,35 +7,54 @@ const addComment = document.getElementById('add-button');
 
 const commentArray = [];
 
-const firstName = 'robin';
-const lastName = 'smits';
-const userComment = 'ik vind dit goed'
+let firstName = 'robin';
+let lastName = 'smits';
+let userComment = 'ik vind dit goed'
 
 riversButton.addEventListener('click', changeMapToRivers);
 damsButton.addEventListener('click', changeMapTodams);
 areaButton.addEventListener('click', changeMapToarea);
-basicButton.addEventListener('click',changeMapToBasic)
+basicButton.addEventListener('click', changeMapToBasic)
 addComment.addEventListener('click', addComments);
 
-function changeMapToRivers (){console.log('rivers works')};
+function changeMapToRivers() { console.log('rivers works') };
 
-function changeMapToBasic (){console.log('Basic works')};
+function changeMapToBasic() { console.log('Basic works') };
 
-function changeMapTodams (){console.log('dams works')};
+function changeMapTodams() { console.log('dams works') };
 
-function changeMapToarea (){console.log('area works')};
-function addComments (){console.log('comment works')}
+function changeMapToarea() { console.log('area works') };
+function addComments() {
+    console.log('comment works');
+    inputFirstName = document.getElementById('fname');
+    inputLastName = document.getElementById('lname');
+    inputComment = document.getElementById('comments');
+    firstName = inputFirstName.value;
+    lastName = inputLastName.value;
+    comment = inputComment.value;
+    addCommentsLayout(firstName, lastName, comment);
+}
 
-function addCommentsLayout (){
+function addCommentsLayout(firstName, lastName, comment) {
     commentDiv = document.createElement('div');
     src = document.getElementById('displayUserInput');
+    commentDiv.classList.add('userCommentInput');
     src.appendChild(commentDiv);
     //p
     commentUserName = document.createElement('p');
-    commentUserName.innerHTML = `${firstName} ${lastName}`
+    
+    commentUserName.setAttribute('id', 'commmentName');
     commentDiv.appendChild(commentUserName);
-    //p
-    displayUserComment = document.createElement('p');
-    displayUserComment = userComment;
-    commentDiv.appendChild(displayUserComment);
+    //strong
+    commentUserNameStrong = document.createElement('strong');
+    commentUserNameStrong.innerHTML = `${firstName} ${lastName}`;
+    commentUserName.appendChild(commentUserNameStrong);
+    //hr
+    commentHr = document.createElement('hr');
+    commentDiv.appendChild(commentHr);
+    //p commentsection
+    displayComment = document.createElement('p');
+    displayComment.innerHTML = `${comment}`;
+    displayComment.classList.add('UserComment')
+    commentDiv.appendChild(displayComment);
 };
