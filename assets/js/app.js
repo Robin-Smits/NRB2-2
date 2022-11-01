8b79547c3744699265ae32b6c82b94f9c8a3394f
Robin-Smits <smit0141@hz.nl>
2022-11-01 09:27:59 +0100

    layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,11 @@ console.log('hello');
 const riversButton = document.getElementById('riversBTN');
 const damsButton = document.getElementById('riversBTN');
 const areaButton = document.getElementById('riversBTN');
-const addComment = document.getElementById('comments')
+const basicButton = document.getElementById('basicBTN');
+const addComment = document.getElementById('add-button');
+
 const commentArray = [];
+
 const firstName = 'robin';
 const lastName = 'smits';
 const userComment = 'ik vind dit goed'
@@ -11,11 +14,19 @@ const userComment = 'ik vind dit goed'
 riversButton.addEventListener('click', changeMapToRivers);
 damsButton.addEventListener('click', changeMapTodams);
 areaButton.addEventListener('click', changeMapToarea);
+basicButton.addEventListener('click',changeMapToBasic)
+addComment.addEventListener('click', addComments);
 
 function changeMapToRivers (){console.log('rivers works')};
+
+function changeMapToBasic (){console.log('Basic works')};
+
 function changeMapTodams (){console.log('dams works')};
+
 function changeMapToarea (){console.log('area works')};
-function addComments (firstName, lastName, userComment){
+function addComments (){console.log('comment works')}
+
+function addCommentsLayout (){
     commentDiv = document.createElement('div');
     src = document.getElementById('displayUserInput');
     src.appendChild(commentDiv);
@@ -27,5 +38,4 @@ function addComments (firstName, lastName, userComment){
     displayUserComment = document.createElement('p');
     displayUserComment = userComment;
     commentDiv.appendChild(displayUserComment);
-}
-addComments ();
+};
